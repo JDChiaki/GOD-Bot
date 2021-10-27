84925d91ba84e86a0fb7ede85fd55ad22a1a4a60
JD_Chiaki <mamiyachiyaki@gmail.com>
2021-10-27 12:58:02 +0000

    from Cat bot to God Bot

--- main.py
+++ main.py
@@ -1,30 +1,23 @@
 import os
 import discord
-import random
+from discord.ext import commands
+from role import Role
 from keep_alive import keep_alive
 
-client = discord.Client()
-res = open('res.txt').read().splitlines()
-kws = open('kws.txt').read().splitlines()
-cts = open('cts.txt').read().splitlines()
-rms = res + cts
+intents = discord.Intents(messages=True, members=True, guilds=True, reactions=True)
+bot = commands.Bot(command_prefix='god! ', case_insensitive=True, help_command=None, intents=intents)
 
-@client.event
+@bot.event
 async def on_ready():
-	print('We have logged in as {0.user}'.format(client))
+	print(f'We have logged in as {bot.user}')
 
-@client.event
-async def on_message(message):
-	if message.author == client.user:
-		return
-	
-	msg = message.content
 
-	if any(word in msg for word in kws):
-		await message.channel.send(random.choice(rms))
-	
-	if msg.startswith('ca?t '):
-		await message.channel.send(random.choice(cts))
+async def setup():
+    await bot.wait_until_ready()
+    bot.add_cog(Role(bot))
 
+bot.loop.create_task(setup())
+
+keep_alive()
 my_secret = os.environ['TOKEN']
-client.run(my_secret)
+bot.run(my_secret)
---
+++ role.py
@@ -0,0 +1,41 @@
+import discord
+from discord.ext import commands
+import random
+
+
+class Role(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    @commands.has_permissions(administrator=True)
+    async def weeb(self, ctx):
+        members = ctx.guild.members
+        role = ctx.guild.get_role(876300081521905725)
+        for member in members:
+            if len(member.roles) == 1:
+                await member.add_roles(role)
+                await ctx.send(member.mention)
+        await ctx.send(f'**You all got `{role.name}` role! Congratulations!**')
+
+    @commands.command(aliases=['r'])
+    @commands.has_permissions(administrator=True)
+    async def role(self, ctx, member: discord.Member, role: discord.Role):
+        if role in member.roles:
+            return await ctx.send(f'**{member.display_name}**\'s already had **{role.name}** role')
+        await member.add_roles(role)
+        t = f'{member.mention}, おめでとうございます！\nあなたは **{role.name}** の役割を果たしました.\n' \
+            f':confetti_ball: :partying_face: :white_flower: \n' \
+            f'{member.mention}, congratulations!\nYou got **{role.name}** role.'
+        with open('./gifs/omedetou.txt', 'r') as f:
+            gif = [line.rstrip() for line in f.readlines()]
+        await ctx.send(t)
+        await ctx.send(random.choice(gif))
+
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx: commands.Context, error: commands.CommandError):
+        if isinstance(error, commands.MissingPermissions):
+            with open('./gifs/howdareyou.txt', 'r') as f:
+                gif = [line.rstrip() for line in f.readlines()]
+            await ctx.send(f'**{ctx.author.mention}! Only Admin can use me! How dare you!!!**')
+            await ctx.send(random.choice(gif))
